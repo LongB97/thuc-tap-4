406c2cc2e28d89e8eb3b15d92fa6e53c0d9f6186
LongB97 <47552979+LongB97@users.noreply.github.com>
2019-02-27 14:08:19 +0700

    push

---
+++ thuc-tap/application/views/frontend/modules/slider.php
@@ -0,0 +1,18 @@
+<div id="carousel-id" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-id" data-slide-to="0" class=""></li>
+        <li data-target="#carousel-id" data-slide-to="1" class="active"></li>
+        <li data-target="#carousel-id" data-slide-to="2" class="active1"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="item">
+            <img src="<?php echo base_url() ?>public/images/HAPPY NEW YEAR.jpg">
+        </div>
+        <div class="item active">
+            <img src="<?php echo base_url() ?>public/images/Recovered.jpg">
+        </div>
+        <div class="item active1">
+            <img src="<?php echo base_url() ?>public/images/checkin.jpg">
+        </div>
+    </div>
+</div>
